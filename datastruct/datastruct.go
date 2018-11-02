83573e3ad111260a81eca8f555745129f1334d66
zs <zs@163.com>
2018-11-02 14:06:51 +0800

    add

---
+++ datastruct/datastruct.go
@@ -0,0 +1,16 @@
+package datastruct
+
+import (
+)
+
+
+const NULLSTRING = ""
+const NULLID = -1
+
+type TestData struct {
+	 UserName string
+	 Avatar string
+}
+
+
+
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"farm/datastruct"
+)
+
+
+func getTest(r *gin.Engine) {
+	 data:=new(datastruct.TestData)
+	 data.UserName = "user1"
+	 data.Avatar="avatar1"
+	 r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+		  "data": data,
+		})
+	})
+}
+
+//添加版本号
+func version()gin.HandlerFunc{
+	return func(c *gin.Context) {
+		c.Writer.Header().Add("Version", "1.0")
+		c.Next()
+	}
+}
+
+//跨域
+func cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Next()
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(cors())
+	r.Use(version())
+	getTest(r)
+	r.Run("192.168.0.176:8080") // listen and serve on 0.0.0.0:8080
+}
+
