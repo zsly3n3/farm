1d89767e7cb508ab5c20e2b4dd913cc4039e16d2
zs <zs@163.com>
2018-11-05 14:41:24 +0800

    add

--- cache/handle.go
+++ cache/handle.go
@@ -3,27 +3,16 @@ package cache
 import (
 	//"github.com/gomodule/redigo/redis"
 	"farm/datastruct"
-	"farm/log"
+	//"farm/log"
 )
 
 func (handle *CACHEHandler) GetPlayerData(code string) (*datastruct.PlayerData,bool){
+	isExist:=false
+	var rs *datastruct.PlayerData
 	ilen, err := handle.redisClient.Get().Do("hlen", code)
-    if err != nil {
-	   log.Debug("hexist failed", err.Error())
-    } else {
-	   log.Debug("count:%d", ilen.(int64))
-    }
-     
-
-	// result, err := redis.Values(handle.redisClient.Get().Do("hgetall", code))
-    // if err != nil{
-	//   isExist = false
-	//   log.Debug("CACHEHandler GetPlayerData err:%s",err.Error())
-	// } else {
-	//   log.Debug("all keys and values are:")
-    //   for _, v := range result {
-    //     log.Debug("%s ", v.([]byte))
-    //   }	
-	// }
-	return nil,false
+    if err == nil || ((ilen.(int64)) > 0){
+	   isExist = true
+	   //add
+    } 
+	return rs,isExist
 }
--- db/handle.go
+++ db/handle.go
@@ -5,5 +5,13 @@ import(
 )
 
 func (handle *DBHandler) GetPlayerData(code string) (*datastruct.PlayerData,bool){
-	 return nil,false
+	 isExist:=false
+	 var rs *datastruct.PlayerData
+	 user := new(datastruct.UserInfo)
+	 has, _ := handle.mysqlEngine.Where("identity_id=?", code).Get(user)
+	 if has{
+		isExist = true
+		//add
+	 }
+	 return rs,isExist
 }
