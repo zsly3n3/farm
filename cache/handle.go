c53aa512a0c8296a42c070f2e0e3eb82c4d08239
zs <zs@163.com>
2018-11-05 16:36:48 +0800

    add

--- cache/handle.go
+++ cache/handle.go
@@ -3,7 +3,7 @@ package cache
 import (
 	"github.com/gomodule/redigo/redis"
 	"farm/datastruct"
-	"farm/log"
+	//"farm/log"
 )
 
 func (handle *CACHEHandler) GetPlayerData(code string) (*datastruct.PlayerData,bool){
