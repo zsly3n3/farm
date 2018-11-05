ae5bcfdf76db2e520416589f32b8901a6b26bbde
zs <zs@163.com>
2018-11-05 13:51:27 +0800

    add

--- cache/redis.go
+++ cache/redis.go
@@ -16,14 +16,12 @@ type CACHEHandler struct {
 }
 
 func getRedisPool() *redis.Pool{
-	log.Debug("----aaa---")
     RedisClient := &redis.Pool{
         // 从配置文件获取maxidle以及maxactive，取不到则用后面的默认值
         MaxIdle:     1,//最大的空闲连接数，表示即使没有redis连接时依然可以保持N个空闲的连接，而不被清除，随时处于待命状态。
         MaxActive:   10,//最大的激活连接数，表示同时最多有N个连接
         IdleTimeout: 180 * time.Second,
         Dial: func() (redis.Conn, error) {
-			log.Debug("----bbb---")
 			conn, err := redis.Dial("tcp",DB_IP)
 			errhandle(err)
 			if _, err := conn.Do("AUTH", DB_Pwd); err != nil {		
@@ -34,14 +32,13 @@ func getRedisPool() *redis.Pool{
             return conn, nil
         },
 	}
-	log.Debug("----ccc---")
     return RedisClient
 }
 
 func CreateCACHEHandler()*CACHEHandler {
 	cacheHandler:=new(CACHEHandler)
 	cacheHandler.redisClient = getRedisPool()
-	cacheHandler.redisClient().Get()
+	cacheHandler.redisClient.Get()
 	return cacheHandler
 }
 
