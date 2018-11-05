b58dc630fbf51f7a08bfaea855d2d747c8a55370
zs <zs@163.com>
2018-11-05 16:30:25 +0800

    add

--- cache/handle.go
+++ cache/handle.go
@@ -3,30 +3,51 @@ package cache
 import (
 	"github.com/gomodule/redigo/redis"
 	"farm/datastruct"
-	"farm/log"
+	//"farm/log"
 )
 
 func (handle *CACHEHandler) GetPlayerData(code string) (*datastruct.PlayerData,bool){
 	isExist:=false
 	var rs *datastruct.PlayerData
-	ilen, err := handle.redisClient.Get().Do("hlen", code)
+	conn:=handle.redisClient.Get()
+	ilen, err := conn.Do("hlen", code)
     if err == nil && (ilen.(int64)) > 0{
 	   isExist = true
-	   //add
-    } 
+	   rs = readPlayerData(conn,code)
+	}
+	conn.Close()
 	return rs,isExist
 }
 
 func (handle *CACHEHandler) SetPlayerData(p_data *datastruct.PlayerData) {
 	conn:=handle.redisClient.Get()
+	defer conn.Close()
 	key:=p_data.IdentityId
+	//add
 	_, err := conn.Do("hmset", key, datastruct.GoldField, p_data.GoldCount, datastruct.HoneyField, p_data.HoneyCount, datastruct.IsAuthField, p_data.IsAuth,datastruct.CreatedAtField,p_data.CreatedAt,datastruct.UpdateTimeField,p_data.UpdateTime)
 	if err == nil {
-	   value, err := redis.Values(conn.Do("hmget",key, datastruct.GoldField, datastruct.HoneyField, datastruct.IsAuthField, datastruct.CreatedAtField,datastruct.UpdateTimeField))
-	   if err == nil {
-		for _, v := range value {
-			log.Debug("%s ", v.([]byte))
-		}
+	}
+}
+
+func readPlayerData(conn redis.Conn,key string) *datastruct.PlayerData{
+	rs := new(datastruct.PlayerData)
+	//add
+	value, err := redis.Values(conn.Do("hmget",key, datastruct.GoldField, datastruct.HoneyField, datastruct.IsAuthField, datastruct.CreatedAtField,datastruct.UpdateTimeField))
+	if err == nil {
+	   for index, v := range value {
+		   switch index{
+			 case 0:
+				rs.GoldCount = v.(int64)
+			 case 1:
+				rs.HoneyCount = v.(int64)
+			 case 2:
+				rs.IsAuth = v.(bool)
+			 case 3:
+				rs.CreatedAt = v.(int64)
+			 case 4:
+				rs.UpdateTime = v.(int64)
+		   }
 	   }
 	}
+	return rs
 }
