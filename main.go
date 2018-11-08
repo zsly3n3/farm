c1ab06343649084ae253cd77014548ae88a1de99
zs <zs@163.com>
2018-11-08 17:05:28 +0800

    add

--- main.go
+++ main.go
@@ -12,23 +12,22 @@ var eventHandler *event.EventHandler
 //跨域
 func cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
-		// c.Next()
-
-		method := c.Request.Method
- 
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "true")
- 
-		//放行所有OPTIONS方法
-		if method == "OPTIONS" {
-		   c.AbortWithStatus(204)
-		}
-		// 处理请求
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
 		c.Next()
+
+		// method := c.Request.Method
+		// c.Header("Access-Control-Allow-Origin", "*")
+		// c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		// c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		// c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		// c.Header("Access-Control-Allow-Credentials", "true")
+ 
+		// //放行所有OPTIONS方法
+		// if method == "OPTIONS" {
+		//    c.AbortWithStatus(204)
+		// }
+		// // 处理请求
+		// c.Next()
 	}
 }
 
--- routes/routes.go
+++ routes/routes.go
@@ -93,7 +93,7 @@ func checkToken(c *gin.Context) (string,bool){
 
 func checkVersion(c *gin.Context,eventHandler *event.EventHandler) bool{
 	  //map[string][]string
-		version,isExist:=c.Request.Header["Version"]
+		version,isExist:=c.Request.Header["AppVersion"]
 		tf:=false
 		if isExist && version[0] == eventHandler.Version{
 			 tf = true
