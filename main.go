d967b97c379300ced29e8ffe5a45aafbbbb83293
zs <zs@163.com>
2018-11-08 17:24:09 +0800

    add

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"farm/event"
 	"farm/routes"
+	"strings"
+	"fmt"
+	"net/http"
 )
 
 
@@ -15,23 +18,38 @@ func cors() gin.HandlerFunc {
 		// c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
 		// c.Next()
 
-		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Add("Access-Control-Allow-Headers", "*")
-		c.Next()
+		method := c.Request.Method
 
-		// method := c.Request.Method
-		// c.Header("Access-Control-Allow-Origin", "*")
-		// c.Header("Access-Control-Allow-Headers", "*")
-		// c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-		// c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		// c.Header("Access-Control-Allow-Credentials", "true")
- 
-		// //放行所有OPTIONS方法
-		// if method == "OPTIONS" {
-		//    c.AbortWithStatus(204)
-		// }
-		// // 处理请求
-		// c.Next()
+        origin := c.Request.Header.Get("Origin")
+        var headerKeys []string
+        for k, _ := range c.Request.Header {
+            headerKeys = append(headerKeys, k)
+        }
+        headerStr := strings.Join(headerKeys, ", ")
+        if headerStr != "" {
+            headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
+        } else {
+            headerStr = "access-control-allow-origin, access-control-allow-headers"
+        }
+        if origin != "" {
+            //下面的都是乱添加的-_-~
+            // c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+            c.Header("Access-Control-Allow-Origin", "*")
+            c.Header("Access-Control-Allow-Headers", headerStr)
+            c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+            // c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
+            c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+            // c.Header("Access-Control-Max-Age", "172800")
+            c.Header("Access-Control-Allow-Credentials", "true")
+            c.Set("content-type", "application/json")
+        }
+
+        //放行所有OPTIONS方法
+        if method == "OPTIONS" {
+            c.JSON(http.StatusOK, "Options Request!")
+        }
+
+        c.Next()
 	}
 }
 
