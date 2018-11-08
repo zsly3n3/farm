0b81e33478d0f7892dcb9004b74d180538a12a42
zs <zs@163.com>
2018-11-08 15:47:26 +0800

    add

--- event/event.go
+++ event/event.go
@@ -5,6 +5,7 @@ import(
 	"farm/cache"
 	"time"
 	"farm/datastruct"
+	"farm/log"
 )
 
 type EventHandler struct {
@@ -23,6 +24,9 @@ func CreateEventHandler()*EventHandler{
 	 eventHandler.createTicker(5*time.Minute)
 	 eventHandler.Version = "1.0.0.0"
 	 eventHandler.Plants = eventHandler.dbHandler.GetPlantsMap()
+	 for k,v:=range eventHandler.Plants{
+		 log.Debug("key:%v , value:%v",k,v)
+	 }
 	 return eventHandler
 }
 
