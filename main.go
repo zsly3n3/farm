5ab52c6739ac322b1b90b4fef5c544113089ed29
zs <zs@163.com>
2018-11-08 17:20:56 +0800

    add

--- main.go
+++ main.go
@@ -15,19 +15,23 @@ func cors() gin.HandlerFunc {
 		// c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
 		// c.Next()
 
-		method := c.Request.Method
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, appversion")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "true")
- 
-		//放行所有OPTIONS方法
-		if method == "OPTIONS" {
-		   c.AbortWithStatus(204)
-		}
-		// 处理请求
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Add("Access-Control-Allow-Headers", "*")
 		c.Next()
+
+		// method := c.Request.Method
+		// c.Header("Access-Control-Allow-Origin", "*")
+		// c.Header("Access-Control-Allow-Headers", "*")
+		// c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		// c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		// c.Header("Access-Control-Allow-Credentials", "true")
+ 
+		// //放行所有OPTIONS方法
+		// if method == "OPTIONS" {
+		//    c.AbortWithStatus(204)
+		// }
+		// // 处理请求
+		// c.Next()
 	}
 }
 
