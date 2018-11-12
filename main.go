69717eec4f815d9e13a1e2c83c1ed0510cd45296
zs <zs@163.com>
2018-11-12 13:26:41 +0800

    add

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"farm/event"
 	"farm/routes"
-	"net/http"
+	//"net/http"
 )
 
 
@@ -16,18 +16,18 @@ func cors() gin.HandlerFunc {
 		// c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
 		// c.Writer.Header().Add("Access-Control-Allow-Headers", "appversion,apptoken")
 		// c.Next()
-        method := c.Request.Method
+        // method := c.Request.Method
  
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Appversion, Apptoken")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Allow-Methods", "POST, GET")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		c.Header("Access-Control-Allow-Credentials", "true")
  
 		//放行所有OPTIONS方法
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-		}
+		// if method == "OPTIONS" {
+		// 	c.AbortWithStatus(http.StatusNoContent)
+		// }
 		// 处理请求
 		c.Next()
 	}
