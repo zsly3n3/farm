3285cd554db2f37d60b4bf48ffc87176be8647cf
cxp <cxp@pc.com>
2018-11-20 00:50:50 +0800

    add

--- cache/handle.go
+++ cache/handle.go
@@ -102,17 +102,17 @@ func (handle *CACHEHandler) DeletedKeys(keys []string, petbars map[datastruct.An
 	conn := handle.GetConn()
 	defer conn.Close()
 	conn.Send("MULTI")
-
 	for _, v := range keys {
 		conn.Send("del", v)
-		for k, _ := range petbars {
-			petbarStr := fmt.Sprintf("petbar%d", int(k))
-			conn.Send("hdel", petbarStr, v)
-		}
-		for k, _ := range soils {
-			soilStr := fmt.Sprintf("soil%d", int(k))
-			conn.Send("hdel", soilStr, v)
-		}
+		// for k, _ := range petbars {
+		// 	petbarStr := fmt.Sprintf("petbar%d", int(k))
+		// 	conn.Send("hdel", petbarStr, v)
+		// }
+		// for k, _ := range soils {
+		// 	soilStr := fmt.Sprintf("soil%d", int(k))
+		// 	conn.Send("hdel", soilStr, v)
+		// }
+		log.Debug("del key:%v", v)
 	}
 
 	_, err := conn.Do("EXEC")
